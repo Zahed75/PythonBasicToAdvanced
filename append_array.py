bba178a9f22467358cde7a6441b59749ebe58b6a
Zahed75 <Zahedhasan.CS@gmail.com>
2022-02-23 02:46:04 +0600

    user input with array

---
+++ append_array.py
@@ -0,0 +1,11 @@
+a = [103, 105, 23, 343, "Mariam"]
+
+print("Before appending")
+
+for x in a:
+    print(x)
+
+a.append("Zahed Hasan")
+print("after appending")
+for z in a:
+    print(z)
---
+++ user_array_input.py
@@ -0,0 +1,7 @@
+a = []
+n = int(input("Enter your Elements:"))
+for i in range(n):
+    a.append(int(input("Enter Element:")))
+
+for x in a:
+    print(x)
